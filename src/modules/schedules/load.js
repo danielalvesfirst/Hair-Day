e69c7b2ab5525d707c65eb1718120c9af52cb07b
danielalvesfirst <danielalvesticoo@gmail.com>
2026-09-26 08:37:31 -0300

    aplicando horarios no agendamentos, mas contém bugs

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,5 +1,6 @@
 import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load"
+import { schedulesShow } from "../schedules/show.js"
 
 //seleciona o input de data.
 const selectedDate = document.getElementById("date")
@@ -12,7 +13,7 @@ export async function schedulesDay() {
   const dailySchedules = await scheduleFetchByDay({ date })
 
   // Exibe os agendamentos
-  
+  schedulesShow({ dailySchedules })
 
   // renderiza as horas disponíveis.
   hoursLoad({ date })
